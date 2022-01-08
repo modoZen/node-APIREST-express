4bf8bcb83d9130df4be5e77d597c63bae75bb099
Max <mdiazn@uni.pe>
2022-01-08 11:44:32 -0500

    clase 8

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const faker = require('faker');
+const routerApi = require('./routes');
 
 const app = express();
 
 const port = 3000;
+
+app.use(express.json());
 
 app.get('/',(req, res)=>{
   res.send('Hola mi server en express');
@@ -13,41 +15,9 @@ app.get('/nueva-ruta',(req, res)=>{
   res.send('Hola soy una nueva ruta');
 });
 
-app.get('/products',(req, res)=>{
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(),10),
-      image: faker.image.imageUrl(),
-    });
+routerApi(app);
 
-  }
-  res.json(products);
-});
 
-app.get('/products/:id',(req, res)=>{
-  const { id } = req.params;
-  res.json({
-    id,
-    name: "Producto 1",
-    price: 1000
-  })
-});
-
-app.get('/users', (req, res)=>{
-  const { limit, offset } = req.query;
-  if(limit && offset){
-    res.json({
-      limit,
-      offset
-    })
-  } else {
-    res.send('No hay parámetros');
-  }
-})
 
 app.get('/categories/:categoryId/products/:productId',(req, res)=>{
   const { categoryId, productId } = req.params;
