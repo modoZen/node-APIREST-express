a34f7f74bf180dff7a4e39b4cb643076d9ea02e7
Max <mdiazn@uni.pe>
2022-01-07 22:00:00 -0500

    clase 6 y 7 get

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const faker = require('faker');
 
 const app = express();
 
@@ -12,12 +13,49 @@ app.get('/nueva-ruta',(req, res)=>{
   res.send('Hola soy una nueva ruta');
 });
 
-app.get('/productos',(req, res)=>{
+app.get('/products',(req, res)=>{
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(),10),
+      image: faker.image.imageUrl(),
+    });
+
+  }
+  res.json(products);
+});
+
+app.get('/products/:id',(req, res)=>{
+  const { id } = req.params;
   res.json({
+    id,
     name: "Producto 1",
     price: 1000
-  });
+  })
 });
+
+app.get('/users', (req, res)=>{
+  const { limit, offset } = req.query;
+  if(limit && offset){
+    res.json({
+      limit,
+      offset
+    })
+  } else {
+    res.send('No hay parámetros');
+  }
+})
+
+app.get('/categories/:categoryId/products/:productId',(req, res)=>{
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId
+  })
+})
 
 app.listen(port, ()=>{
   console.log(`My port ${port}`);
