2a430e9b4e41ab151b59e184962836bce2603de8
Max <mdiazn@uni.pe>
2022-01-14 23:06:05 -0500

    clase 17 y 18

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routerApi = require('./routes');
 
+const { logErrors, errorHandler, boomErrorHandler } = require('./error.handler');
+
 const app = express();
 
 const port = 3000;
@@ -16,6 +18,10 @@ app.get('/nueva-ruta',(req, res)=>{
 });
 
 routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
 
 
